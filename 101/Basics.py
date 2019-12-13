290427fa961f7b01cfd808f0dfd04a04bb317c49
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-12 22:36:06 -0800

    Add basics exercices

---
+++ 101/Basics.py
@@ -0,0 +1,46 @@
+# https://www.learnpython.org/
+
+# Hello, World!
+print("Hello Xavier") #Hello Xavier
+
+# Indentation
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+    # x is 1.
+
+
+# Numbers
+myint = 7
+print(myint)
+# float
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+# Strings
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+# Additions
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+# Multi assignements
+a, b = 3, 4
+print(a,b)
