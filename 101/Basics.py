c31dcb744e82e58382e263c87cec1303d6d64581
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-17 22:11:04 -0800

    Update basics

--- 101/Basics.py
+++ 101/Basics.py
@@ -48,3 +48,27 @@ a, b = 3, 4
 print(a,b)
 
 
+# lists
+x = object()
+y = object()
+
+## TODO: change this code
+x_list = [x,x,x,x,x,x,x,x,x,x]
+y_list = [y,y,y,y,y,y,y,y,y,y]
+big_list = x_list+ y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+## testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
+
+
+# FORMAT
+
+'{0} and {1} and {2}'.format("premier", "deuxieme", "troisieme")
+#'premier and deuxieme and troisieme'
