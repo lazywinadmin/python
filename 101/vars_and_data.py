dddea379f44aa0671d89e50d5ba177bcccdddf32
lazywinadmin <fxcat@lazywinadmin.com>
2020-02-24 15:58:17 -0800

    add vars and data playground

---
+++ 101/vars_and_data.py
@@ -0,0 +1,10 @@
+# Strings
+name="John"
+age=35
+is_male = True
+
+print("There once was a man named " + name +", ")
+print("he was "+ str(age) + ",")
+print("He really liked the name " + name + ",")
+print("But didnt like being " + str(age) + ".")
+
