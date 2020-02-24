74e423662ec19f8ebe68d14d6f28ff3d4d2e3a17
lazywinadmin <fxcat@lazywinadmin.com>
2020-02-24 15:58:27 -0800

    add string

---
+++ 101/strings.py
@@ -0,0 +1,28 @@
+# New line
+print("Hello\nWorld!")
+
+# Escape characters
+print("Hello \"World!\"")
+
+# Concat
+phrase="Hello"
+print(phrase + " World!")
+
+# Using function/methods
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase)) # lenght
+
+# index
+print(phrase[1]) # grab one of the character
+print(phrase.index("H")) #find the index for an item
+
+# Replace
+phrase="Hello people of the "
+print(phrase.replace("Hello","Bonjour"))
+
+
+
+
