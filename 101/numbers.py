eb38fe14d1a4dd845d21efec661b724563278f4e
lazywinadmin <fxcat@lazywinadmin.com>
2020-02-24 16:07:03 -0800

    add numbers

---
+++ 101/numbers.py
@@ -0,0 +1,36 @@
+# simple operation
+print(1.2)
+print(3+5.4*9)
+print((3+5.4)*9)
+# Remainder
+print(10 % 3) # modulus operator
+
+# using var
+num=5
+print(num)
+print(str(num))
+
+# Absolute value of a number
+num=-5
+print(abs(num)) # remove the minus
+
+# power
+print(pow(4,6))
+
+# max (largest value)
+print(max(4,6))
+# min
+print(min(4,6))
+
+#round
+print(round(3.2)) #lower
+print(round(3.7)) #round up
+print(round(3.5)) #round up
+
+# importing more functionality
+from math import *
+print(floor(3.7)) # grab the lowest number
+print(ceil(3.7)) # grab the up number
+print(sqrt(36))
+
+
