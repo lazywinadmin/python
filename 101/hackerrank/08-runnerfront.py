36a086c3ae62a6379b476ab5749cbd6be331034e
lazywinadmin <fxcat@lazywinadmin.com>
2020-04-19 15:31:59 -0700

    add runnerfront challenge

---
+++ 101/hackerrank/08-runnerfront.py
@@ -0,0 +1,14 @@
+# Find the Runner-Up Score (second position)
+#
+# Given the participants' score sheet for your University Sports Day,
+# you are required to find the runner-up score. You are given  scores.
+# Store them in a list and find the score of the runner-up.
+#
+# python3 -m pip install numpy
+#
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+
+    print(sorted(list(set(arr)))[-2])
